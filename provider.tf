972e899515639bc125e69bfa2f4d55dc3ef90826
adeshthorat <adesh.th03@gmail.com>
2025-12-21 22:08:21 +0530

    updated provider block

--- provider.tf
+++ provider.tf
@@ -1,16 +1,20 @@
 terraform {
+  required_version = ">= 1.5"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.8.0"
+      version = "~> 6.8"
     }
+  }
+
+  backend "s3" {
+    bucket         = "terraform-user-statefile-artifacts"
+    key            = "tfstate/terraform.tfstate"
+    region         = "us-east-1"
   }
 }
 
-terraform {
-  backend "s3" {
-    bucket = "terraform-user-statefile-artifacts"
-    key    = "tfstate/terraform.tfstate"
-    region = "us-east-1"
-  }
+provider "aws" {
+  region = "us-east-1"
 }
