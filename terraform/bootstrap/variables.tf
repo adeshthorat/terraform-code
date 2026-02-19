254dc8fd2c5dd0ed694ce8fdbe462dad4cf0fab4
adesh <adesh.thorat@gmail.com>
2026-02-19 14:44:35 +0530

    Add AWS environment variables to Terraform workflows and remove unused lock table variable

--- terraform/bootstrap/variables.tf
+++ terraform/bootstrap/variables.tf
@@ -8,8 +8,3 @@ variable "state_bucket_name" {
   description = "The name of the S3 bucket to store Terraform state"
   type        = string
 }
-
-variable "lock_table_name" {
-  description = "The name of the DynamoDB table for state locking"
-  type        = string
-}
