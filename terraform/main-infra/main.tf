d3e395789d2256db48126b8a1cc9280df994b8dc
adesh <adesh.thorat@gmail.com>
2026-03-04 12:20:09 +0530

    Bastion access allowed for port 8001

--- terraform/main-infra/main.tf
+++ terraform/main-infra/main.tf
@@ -56,3 +56,13 @@ resource "aws_security_group_rule" "allow_DBServerAccess" {
   cidr_blocks     = ["10.0.0.0/8"]
   description     = "Allow outbound traffic on port 1443 (HTTP)"
 }
+
+resource "aws_security_group_rule" "allow_BastionAccess" {
+  type            = "ingress"
+  from_port       = 8001
+  to_port         = 8001
+  protocol        = "tcp"
+  security_group_id = data.aws_security_group.DB_access_sg.id
+  cidr_blocks     = ["10.0.0.0/8"]
+  description     = "Allow inbound Bastion access for Port 8001 (HTTP)"
+}
